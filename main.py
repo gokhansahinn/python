616bf1e5efde53c09449a73759c16ce91767c395
gokhansahinn <gkhnsahin4848@gmail.com>
2022-09-28 15:16:43 +0300

    module example added

---
+++ main.py
@@ -0,0 +1,17 @@
+import mod
+import random
+
+value=mod.number
+print(value)
+value=mod.Numbers
+print(value)
+print(random.choice(value))
+
+value=mod.person
+print(value['name'])
+
+mod.func(9)
+
+p=mod.Person()
+
+p.speak()
---
+++ mod.py
@@ -0,0 +1,22 @@
+'''
+My Module created.
+'''
+print('Module added.')  #Main modül eklendiğinde kod direk çalışmaktadır.
+number=10
+
+Numbers=[1,2,3,4]
+def func(x):
+    '''
+    Display value that taken from user.
+    '''
+    print(f'User value: {x}')
+
+person={
+    'name':'Gökhan',
+    'age': 25,
+    'jobs':'elektric and electronic engineer'
+}
+
+class Person:
+    def speak(self):
+        print('I am speaking')
